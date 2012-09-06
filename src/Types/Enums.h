0214793bd2610799a84840862540653cd7436490
Tom Thorogood <tom@tomthorogood.com>
2012-09-06 14:36:04 -0400

    Moar documentation.

--- src/Types/Enums.h
+++ src/Types/Enums.h
@@ -3,7 +3,11 @@
 
 #include <stdint.h>
 
-
+/*!\brief This namespace contains all enumerated types within Mogu. For
+ * the ease of maintainability, they are split into different files. This does
+ * cause some problems with Eclipse if editing the enums or their parsers, so
+ * if Eclipse starts going insane, just rebuild the index.
+ */
 namespace Enums{
 
 #include <Types/E_Signals.h>
