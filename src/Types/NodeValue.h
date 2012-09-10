c149fa744b5938dbb2dd334699cc34485ba86697
Tom Thorogood <tom@tomthorogood.com>
2012-09-10 09:54:57 -0400

    Add docummentation.

--- src/Types/NodeValue.h
+++ src/Types/NodeValue.h
@@ -24,33 +24,61 @@ enum ReadType
     float_value		=0x3
 };
 
+/*!\brief Since a NodeValue can only be int, float, or string, the NumericUnion
+ * consolidates the int/float possibilities into one type to save a hair of
+ * memory. The string is held inside the NodeValue class itself.
+ */
 union NumericUnion
 {
 	int		as_int;
 	float 	as_float;
 };
 
+/*!\brief The NodeValue class holds a parsed node value of any valid type,
+ * currently limited to int/float/string. This allows for added polymorphic
+ * capabilities of any classes that use node values in their logic.
+ */
 class NodeValue
 {
+	/*!\brief one of the possible types of values */
     ReadType __type;
+
+    /*!\brief If the value is a string, it is stored here. */
     std::string as_string;
+
+    /*!\brief If the value is numeric, it is stord here. */
     NumericUnion* __numerics;
 
 public:
     NodeValue();
 
-    /* Copies the value of another node value into this one. */
+    /*!\brief Copies the value of another node value into this one. */
     NodeValue(NodeValue*);
+
+    /*!\brief deletes the pointer to the NumericUnion*/
     virtual ~NodeValue();
 
+    /*!\brief Sets the string as well as __type */
     void setString(std::string val);
+
+    /*!\brief Sets an int as well as __type */
     void setInt(int val);
+
+    /*!\brief Sets a float as well as __type */
     void setFloat(float val);
+
+    /*!\brief If the type is `string_value`, returns a string. */
     std::string getString();
+
+    /*!\brief If the type is `int_value`, returns the int.
     int getInt();
+
+    /*!\brief If the type is `float_value`, returns the float.
     float getFloat();
+
+    /*!\brief Returns the type so the correct data can be extracted. */
     ReadType getType();
-};
+}; // end NodeValue
 
 } // namespace nodes
 
