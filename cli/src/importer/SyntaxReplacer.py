64ad62e4e71f2fb405b5f58da0acd18908e556bc
Tom Thorogood <tom@tomthorogood.com>
2013-02-28 12:23:17 -0500

    SyntaxReplacer adds references to symbols.

--- cli/src/importer/SyntaxReplacer.py
+++ cli/src/importer/SyntaxReplacer.py
@@ -2,11 +2,61 @@
 # with integral representations
 
 import syntax # Syntax/Integer Map
+from sets import Set
 from pyRex import Consumer
 from pyRex import Lexer
 from regexlib import regexlib # Basic regular expressions
-from lex_functions import everything_until
+from lex_functions import reference_widget
+from lex_functions import reference_data
+from lex_functions import reference_template
+from lex_functions import reference_validator
+from lex_functions import reference_policy
+import SharedData
+import SymbolRegistry
 import re
+
+def is_integer(string):
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+def referenced_types():
+    referenced_types = [
+            syntax.as_integer("widget"),
+            syntax.as_integer("data"),
+            syntax.as_integer("user"),
+            syntax.as_integer("group")
+            ]
+    return referenced_types
+
+def reference(integral_repr, symbol):
+    integral_repr = int(integral_repr)
+    referenceTables = (
+            SymbolRegistry.widgetRegistry,
+            SymbolRegistry.dataRegistry,
+            SymbolRegistry.policyRegistry,
+            SymbolRegistry.policyRegistry
+            )
+    types = referenced_types()
+    for index,repr_type in enumerate(types):
+        if integral_repr == repr_type:
+            referenceTables[index][symbol].append(SharedData.ActiveFile)
+
+def create_references(string):
+    stringlits = re.findall(regexlib["string_literal"], string)
+    
+    # We don't need to test string literals. Therefore, strip them out. 
+    for lit in stringlits:
+        while lit in string:
+            string = string.replace(lit," ")
+    # get only valid tokens
+    tokens = [entry for entry in string.split(" ") if entry]
+    for index,token in enumerate(tokens):
+        if not is_integer(token) and index != 0:
+            if (int(tokens[index-1]) in referenced_types()):
+                reference(tokens[index-1], token)
 
 def find_syntax_tokens(string):
     """
@@ -16,11 +66,11 @@ def find_syntax_tokens(string):
     length first to be sure that the largest inputs are found
     first.
     """
-    ltokens = [key for key in syntax.MoguSyntax]
+    ltokens = [key+"\s+" for key in syntax.MoguSyntax]
     ltokens = sorted(ltokens,key=len)
     ltokens.reverse()
     tokens = "(%s)" % ("|".join(ltokens))
-
+    found = re.findall(tokens,string)
     return re.findall(tokens,string)
 
 def filter_string_literals (string):
@@ -39,8 +89,10 @@ def replace_syntax(string):
     working, literals = filter_string_literals(string)
     tokens = find_syntax_tokens(working)
     for token in tokens:
-        working = working.replace(token,str(syntax.as_integer(token)))
+        working = working.replace(token,str(syntax.as_integer(token))+" ")
     working = working % tuple(literals)
+    working = working.strip()
+    create_references(working)
     return working
 
 if __name__ == "__main__":
