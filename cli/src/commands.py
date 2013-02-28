c39a2bee9a1debb512ec750627c1a92f420eb373
Tom Thorogood <tom@tomthorogood.com>
2013-02-28 13:05:07 -0500

    Purge files no longer required for import

--- cli/src/commands.py
+++ cli/src/commands.py
@@ -1,18 +1,13 @@
 # CLI Parser
 from cli_parser import getCLIArgs
 
-# find-orphans
-from find_orphans import *
-
 # merge-sessions
 from session_merge import merge_all as merge_sessions
 
 # write_config_file
 from config_generator import create_file as create_config
 
-# import
-# import-directory
-from importdb import *
+from importer import mogu_import
 
 # export
 # export-widget
@@ -28,16 +23,5 @@ from newfile import newfile
 # purge-sessions
 from session_purge import purge_sessions
 
-# delete
-from delete_key import delete_key as delete
-
 # init
 from init import init
-
-# relate
-from relationships import relate
-
-# find-missing-children
-from find_missing_children import find_missing_children
-
-from check_syntax import check_syntax
