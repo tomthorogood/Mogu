f81ddb20eb459cef3954c783a5333a65de021ceb
Cameron Wong <reolyze@gmail.com>
2013-03-04 11:18:12 -0800

    new files for attribute parser

---
+++ src/Parsers/AttributeParser.cpp
@@ -0,0 +1,16 @@
+/*
+ * AttributeParser.cpp
+ *
+ *  Created on: March 4th, 2013
+ *      Author: cameron
+ */
+
+#include <Parsers/AttributeParser.h>
+
+namespace Parsers {
+
+AttributeParser::AttributeParser()
+{
+}
+
+}	// namespace Parsers
---
+++ src/Parsers/AttributeParser.h
@@ -0,0 +1,19 @@
+/*
+ * AttributeParser.h
+ *
+ *  Created on: March 4th, 2013
+ *      Author: cameron
+ */
+
+namespace Parsers {
+	
+class AttributeParser
+{
+	public:
+		AttributeParser();
+	
+	private:
+
+}
+
+}	// namespace Parsers
