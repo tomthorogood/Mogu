abaafd5dd978bb0d87293af34c2e3f5ad092e1f7
Cameron Wong <reolyze@gmail.com>
2013-03-13 22:23:50 -0700

    just testing some files

---
+++ src/Parsers/MathParser.cpp
@@ -0,0 +1,16 @@
+/*
+ * MathParser.cpp
+ *
+ *  Created on: March 12th, 2013
+ *      Author: cameron
+ */
+
+#include <Parsers/MathParser.h>
+
+namespace Parsers {
+
+MathParser::MathParser()
+{
+}
+
+}	// namespace Parsers
---
+++ src/Parsers/MathParser.h
@@ -0,0 +1,21 @@
+/*
+ * Math.h
+ *
+ *  Created on: March 12th, 2013
+ *      Author: cameron
+ */
+
+namespace Parsers {
+	
+class MathParser
+{
+	public:
+		MathParser();
+	
+	private:
+		std::vector<std::string> __mathTokens;
+		
+
+}
+
+}	// namespace Parsers
