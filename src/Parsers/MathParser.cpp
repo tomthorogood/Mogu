c6fe514d31362b4ecf4ac479baa02e099f0c0004
Cameron Wong <reolyze@gmail.com>
2013-03-19 01:09:32 -0700

    edited c++ syntax to be consistent with mogu project

--- src/Parsers/MathParser.cpp
+++ src/Parsers/MathParser.cpp
@@ -26,83 +26,73 @@ int MathParser::evaluate(std::vector<std::string>& infixExpression)
 	// temporary operator stack for doing conversion
 	std::stack<int> opStack;
 
-	for(int i=0; i<infixExpression.size(); i++)
-	{
+	for(int i=0; i<infixExpression.size(); i++) {
 		int currToken = stoi(infixExpression[i]);
 
-		if(isOperator(currToken))
-		{
-			switch(currToken)
-			{
-				case MATHSYNTAX_OPENPAREN:
-					opStack.push(currToken);
-					break;
+		if(isOperator(currToken)) {
+			switch(currToken) {
+			case MATHSYNTAX_OPENPAREN:
+				opStack.push(currToken);
+				break;
 
-				case MATHSYNTAX_CLOSEPAREN:
-					while(opStack.top() != MATHSYNTAX_OPENPAREN)
-					{
-						__postfixExpression.push_back(opStack.top());
-						opStack.pop();
-					}
-					opStack.pop();	//remove open parens
-					break;
+			case MATHSYNTAX_CLOSEPAREN:
+				while(opStack.top() != MATHSYNTAX_OPENPAREN) {
+					__postfixExpression.push_back(opStack.top());
+					opStack.pop();
+				}
+				opStack.pop();	//remove open parens
+				break;
 
-				default:
-				//we have a +,-,* or /
-					while(!hasHigherPrecedence(currToken, opStack.top()))
-					{
-						__postfixExpression.push_back(opStack.top());
-						opStack.pop();
-					}
-					opStack.push(currToken);
-					break;
+			default:
+			//we have a +,-,* or /
+				while(!hasHigherPrecedence(currToken, opStack.top())) {
+					__postfixExpression.push_back(opStack.top());
+					opStack.pop();
+				}
+				opStack.push(currToken);
+				break;
 			}
 		}
-		else
-		{
+		else {
 			__postfixExpression.push_back(currToken);
 		}
 	}
 
-	while(!opStack.empty())
-	{
+	while(!opStack.empty()) {
 		__postfixExpression.push_back(opStack.top());
 		opStack.pop();
 	}
 
 	// *** EVALUATE POSTFIX EXPRESSION ***
-	for(int i=0; i<__postfixExpression.size(); i++)
-	{
+	for(int i=0; i<__postfixExpression.size(); i++) {
+
 		int currToken = __postfixExpression[i];
 
-		if(!isOperator(currToken))	//operand
-		{
+		if(!isOperator(currToken)) {	//operand 
 			__operandStack.push(currToken);
 		}
-		else						//operator
-		{
+		else {							//operator
 			int op1 = __operandStack.top();
 			__operandStack.pop();
 			int op2 = __operandStack.top();
 			__operandStack.pop();
 
 			int opResult;
-			switch(currToken)
-			{
-				case MATHSYNTAX_PLUS:
-					opResult = op1 + op2;
-					break;
-				case MATHSYNTAX_MINUS:
-					opResult = op1 - op2;
-					break;
-				case MATHSYNTAX_MULTIPLY:
-					opResult = op1 * op2;
-					break;
-				case MATHSYNTAX_DIVIDE:
-					opResult = op1 / op2;
-					break;
+			switch(currToken) {
+			case MATHSYNTAX_PLUS:
+				opResult = op1 + op2;
+				break;
+			case MATHSYNTAX_MINUS:
+				opResult = op1 - op2;
+				break;
+			case MATHSYNTAX_MULTIPLY:
+				opResult = op1 * op2;
+				break;
+			case MATHSYNTAX_DIVIDE:
+				opResult = op1 / op2;
+				break;
 			}
-			__operandStack.push(opResult);
+		__operandStack.push(opResult);
 		}
 	}
 
