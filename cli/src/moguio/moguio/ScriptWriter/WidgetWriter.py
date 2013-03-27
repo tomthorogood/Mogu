2d4ce9684b090fbb24527aca9dce9dfe5b48ac7e
Tom Thorogood <tom@tomthorogood.com>
2013-03-27 11:28:12 -0400

    Factor classes into less files

--- cli/src/moguio/moguio/ScriptWriter/WidgetWriter.py
+++ cli/src/moguio/moguio/ScriptWriter/WidgetWriter.py
@@ -1,8 +1,109 @@
 import IndentTracker
 from DictWriter import DictWriter
 from ListWriter import ListWriter
-from ChildrenWriter import ChildrenWriter
-from EventWriter import EventWriter
+
+class TriggerWriter(object):
+    """
+        The TriggerWriter requires a trigger and
+        a list of commands, it will generate a When block
+        with the following template:
+
+            when %(trigger)s {
+                %(command)s
+                [%(command)s...]
+            }
+    """
+    def __init__(self, trigger, cmdlist):
+        self.trigger = trigger
+        self.commands = cmdlist
+
+    def __str__(self):
+        IndentTracker.INDENT_LEVEL += 1
+        self.tab = IndentTracker.TAB * IndentTracker.INDENT_LEVEL
+        outer = \
+"""
+%(tab)swhen %(trigger)s {
+%(inner)s
+%(tab)s}
+"""
+        lw = ListWriter(self.commands)
+        self.inner = str(lw)
+        IndentTracker.INDENT_LEVEL -= 1
+        return outer % self.__dict__
+
+class ChildrenWriter(object):
+    """
+        A ChildrenWriter requires a list of child
+        names, and may optionally take the parent's name
+        as well. Passing in the parent's name will 
+        allow the child's name to be truncated if possible
+        for more readable shorthand.
+
+        If widgetfoo:childalpha is passed in, and 'widgetfoo'
+        is identified as the parent, only ':childalpha' will be
+        written in the output.
+    """
+    @staticmethod
+    def truncate_parent(child,parent):
+        keypath = child.split(':')
+        try:
+            pIndex = keypath.index(parent)
+            return ":%s" % ":".join(keypath[pIndex+1:])
+        except ValueError:
+            return child
+
+    def __init__(self, children, parentname=None):
+        self.children = children
+        self.parent = parentname
+
+        if self.parent:
+            self.children = [
+                    ChildrenWriter.truncate_parent(child,self.parent) \
+                            for child in self.children]
+   
+    def __str__(self):
+        IndentTracker.INDENT_LEVEL += 1
+        self.tabs = IndentTracker.TAB * IndentTracker.INDENT_LEVEL
+        outer = \
+"""
+%(tabs)schildren
+%(inner)s
+%(tabs)send children
+"""
+        listWriter = ListWriter(self.children)
+        self.inner = str(listWriter)
+
+        output = outer % self.__dict__
+        IndentTracker.INDENT_LEVEL -= 1
+        return output
+
+class EventWriter(object):
+    """
+        An EventWriter requires a dictionary of 
+        'trigger' : [command, ...] 
+        entries, which it will turn into a standard
+        events block.
+    """
+
+    def __init__(self, eventdict):
+        self.eventdict = eventdict
+
+    def __str__(self):
+        IndentTracker.INDENT_LEVEL += 1
+        self.tab = IndentTracker.TAB * IndentTracker.INDENT_LEVEL
+        outer = \
+"""
+%(tab)sevents
+%(inner)s
+%(tab)send events
+"""
+        self.inner = "" 
+        for trigger in self.eventdict:
+            tw = TriggerWriter(trigger,self.eventdict[trigger])
+            self.inner += str(tw)
+        IndentTracker.INDENT_LEVEL -= 1
+
+        return outer % self.__dict__
 
 class AbstractWidgetWriter(object):
     """
