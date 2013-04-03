743d06776a48bb924066b6918a22a7c3ec7195ed
Tom Thorogood <tom@tomthorogood.com>
2013-04-03 16:24:20 -0400

    Add ContextManager; Update QuerySet to rely on Contexts; Update QuerySet to implement a Queue rather than a vector/iter combo.

---
+++ src/Redis/QuerySet.cpp
@@ -0,0 +1,21 @@
+/* * QuerySet.cpp
+ *
+ *  Created on: Apr 3, 2013
+ *      Author: tom
+ */
+
+#include "QuerySet.h"
+
+namespace Redis {
+
+QuerySet::QuerySet(Context& context)
+{
+    rdb = redisConnect(context.host,context.port);
+    auto select = std::make_shared<Query>(
+        new Query("select %d", context.db_num));
+    appendQuery(select, IGNORE_RESPONSE);
+}
+
+}//namespace Redis
+
+
--- src/Redis/QuerySet.h
+++ src/Redis/QuerySet.h
@@ -1,37 +1,62 @@
+#include <ContextManager.h>
+#include <queue>
 namespace Redis {
 
 class QuerySet 
 {
 private:
-    std::vector <uint8_t> queryflags;
-    int flag_iter =-1; //<! Marks the flags for the last query sent.
-    uint8_t     array_type = 0;
+    //!\brief The flags for each query given to the set
+    std::queue <uint8_t> queryflags;
+
+    uint8_t last_flags;
+
+    //!\brief The type of the reply returned from a Redis query
     int         reply_type = -1;
 
+    //!\brief The type of the array returned from a relevant Redis query
+    uint8_t     array_type = 0;
+
+    //!\brief The context (populated by the Context reference passed in
     redisContext* rdb;
+
+    //!\brief The reply from the last query sent to the Redis context
     redisReply* reply;
 
+    //!\brief Holds the last integer returned, where Redis stated the
+    // response was an integer
     int         reply_int;
+
+    //!\brief Holds the last string returned where Redis stated the
+    // response was a string
     std::string reply_string;
+
+    //!\brief Holds a vector of the last set of responses where REdis
+    // stated the response was a reply_array, and it was determined
+    // that the array was made of strings
     std::vector <std::string> reply_array_str;
+
+    //!\brief holds a vector of the last set of responses where Redis
+    // stated the response was a reply_array, and it was determined
+    // that the array was made of integers.
     std::vector <int> reply_array_int;
 
+
     void* vreply;
-    const char* host;
-    int port;
 
     /*! Executes the next command sent to Redis, and
      * increases the flag iterator. 
      */
     inline redisReply* getNextReply() {
+        queryflags.pop(); //reveal the next flag.
+        last_flags = queryflags.front(); // make it easy to see.
         redisGetReply(rdb,&vreply);
         reply = (redisReply*) vreply; //!< in cpp, have to explicitly cast this.
-        ++flag_iter;
         return reply;
     }
 
-    inline uin8_t responseFlags() {
-        return queryflags[flag_iter];
+    /*!\brief Get the handling flags of the last response received from Redis */
+    inline uint8_t responseFlags() {
+        return queryflags.front();
     }
 
     /*!\brief Based on the type of reply received from Redis,
@@ -95,30 +120,33 @@ private:
     }
 
 public:
-    static const int IGNORE_RESPONSE    = 1;
-    static const int REQUIRE_INT        = 2;
-    static const int REQUIRE_STRING     = 4;
+    static const int IGNORE_RESPONSE        = 1;
+    static const int REQUIRE_INT            = 2;
+    static const int REQUIRE_STRING         = 4;
+
     static const uint8_t ARRAY_TYPE_INT     = 0;
     static const uint8_t ARRAY_TYPE_STR     = 1;
     
     //TODO set flag_iter to 0
-    QuerySet(const char* host, int port);
+    QuerySet(Context& context );
     ~QuerySet();
 
+    /*!\brief Add a query to the command queue. */
     inline void appendQuery(std::shared_ptr<Query> query,uint8_t flags=0) {
         redisvAppendCommand(rdb, query->querystring, query->args);
-        queryflags.push_back(flags);
+        queryflags.push(flags);
     }
-    inline bool hasQueue() { return flag_iter < queryflags.size(); }
+
+    inline bool hasQueue() { return queryflags.size() > 1; }
 
     /*!\brief If no type is expected, the default is to store
      * the value of the reply object, but make no returns. 
      * Will continue to execute statements until a reply is expected.
      */
-    template <> void yieldResponse() {
+    void yieldResponse() {
         do {
             getNextReply();
-        } while ( (queryflags[flag_iter] & IGNORE_RESPONSE) && hasQueue() );
+        } while ( (last_flags & IGNORE_RESPONSE) && hasQueue() );
         assignReply();
     }
 
@@ -130,13 +158,13 @@ public:
     template <int> int yieldResponse() {
         do {
             getNextReply();
-        } while ( (queryflags[flag_iter] & IGNORE_RESPONSE) && hasQueue() );
+        } while ( (last_flags & IGNORE_RESPONSE) && hasQueue() );
 
         assignReply();
         
         if (reply_type == REDIS_REPLY_STRING) {
-            if (queryflags[flag_iter] & REQUIRE_INT) return 0;
-            return std::atoi(reply_str.c_str());
+            if (last_flags & REQUIRE_INT) return 0;
+            return std::atoi(reply_string.c_str());
         }
         return reply_int;
     }
@@ -149,24 +177,29 @@ public:
     template <std::string> std::string yieldResponse() {
         do {
             getNextReply();
-        } while ( (queryflags[flag_iter] & IGNORE_RESPONSE) && hasQueue() );
+        } while ( (last_flags & IGNORE_RESPONSE) && hasQueue() );
         assignReply();
 
         if (reply_type == REDIS_REPLY_INTEGER) {
-            if (queryflags[flag_iter] & REQUIRE_STR) return "";
+            if (last_flags & REQUIRE_STRING) return "";
             return std::to_string(reply_int);
         }
-        return reply_str;
+        return reply_string;
     }
 
+    /*!\brief Forces the return of a boolean response,
+     * and continues to execute redis commands until the response
+     * is not ignored. If the REQUIRE_INT flag is set, and the responxse
+     * is a string, false will be returned.
+     */
     template <bool> bool yieldResponse() {
         do {
             getNextReply();
-        } while ((queryflags[flag_iter] & IGNORE_RESPONSE) && hasQueue() );
+        } while ((last_flags & IGNORE_RESPONSE) && hasQueue() );
         assignReply();
         
         if (reply_type == REDIS_REPLY_STRING) {
-            if (queryflags[flag_iter] & REQUIRE_INT) return false;
+            if (last_flags & REQUIRE_INT) return false;
             if (reply_string != "") return true;
             return false;
         }
@@ -180,28 +213,48 @@ public:
         else return false;
     }
 
-    template <std::vector <int>> yieldResponse() {
+    /*!\brief Forces the return of a vector of integers,
+     * and continues to execute redis commands until the response is not
+     * ignored.
+     */
+    template <std::vector <int>>
+        std::vector <int>& yieldResponse() {
         do {
             getNextReply();
-        } while ((queryflags[flag_iter] & IGNORE_RESPONSE) && hasQueue() );
+        } while ((last_flags & IGNORE_RESPONSE) && hasQueue() );
         assignReply();
         return reply_array_int;
     }
 
-    template <std::vector <std::string>> yieldResponse() {
+    /*!\brief Roces the return of a vector of strings,
+     * and continues to execute redis commands until the response is
+     * not ignored.
+     */
+    template <std::vector <std::string>>
+        std::vector<std::string>& yieldResponse() {
         do {
             getNextReply();
-        } while ((queryflags[flag_iter] & IGNORE_RESPONSE) && hasQueue() );
+        } while ((last_flags & IGNORE_RESPONSE) && hasQueue() );
         assignReply();
 
         return reply_array_str;
     }
 
+    //!\brief The last reply type received.
     inline int replyType() { return reply_type;}
-    inline std::string replyString() { return reply_str;}
+
+    //!\brief The last reply string received.
+    inline std::string replyString() { return reply_string;}
+
+    //!\brief The last reply integer received.
     inline int replyInt() { return reply_int;}
-    inline std::vector& <int> replyIntArray() { return reply_array_int;}
-    inline std::vector& <std::string> replyStringArray() { return reply_array_str;}
+
+    //!\brief The last reply array of integers received.
+    inline std::vector <int>& replyIntArray()
+        { return reply_array_int;}
+    //!\brief The last reply array of strings received.
+    inline std::vector <std::string>& replyStringArray()
+        { return reply_array_str;}
 };
 
 }//namespace Redis
