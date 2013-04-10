40a22e4db42c919829be7d1659a2424b9066458a
Tom Thorogood <tom@tomthorogood.com>
2013-04-10 14:17:43 -0400

    Fix issues with keywords that contain more than one word; add easier ways to debug MoguStrings.

--- cli/src/moguio/moguio/FileImporter.py
+++ cli/src/moguio/moguio/FileImporter.py
@@ -15,20 +15,63 @@ def filter_comment(string):
 def nonempty(string):
     return len(string.strip()) > 0 
 
-def stage(filename, verbal=False):
+def preprocess(text,verbal=False):
+    global current_file_input
+    current_file_input = text
+    if not isinstance(text,list):
+        text = text.split("\n")
+
+    assert(isinstance(text,list))
+    text = [ line.strip() for line in text ]# Remove all leading/trailing whitespace from each line
+    text = filter(nonempty,text)            # Only preserve nonempty lines
+    text = filter(filter_comment,text)      # Only preserve non-comments
+    iterindex = 0
+    step = "Text to MoguString:Script"
+    try:
+        # Convert all strings to MoguStrings
+        for index,line in enumerate(text):
+            iterindex = index
+            ms = MoguString("script",line)
+            text[index] = ms
+    
+        step = "MoguString:Script to MoguString:Integral"
+
+        # Translate all strings to integral syntax
+        for index,line in enumerate(text):
+            assert(isinstance(line,MoguString))
+            iterindex = index
+            line.translate('integral')
+            line.active('integral')
+    except Exception as e:
+        errMsg = """
+    %s
+
+Could not parse text: 
+    %s
+
+MoguString Attributes:
+    String Literals: 
+        %s
+
+    Current Temp:
+        %s
+
+Step:
+    %s
+""" %(e, text[iterindex], text[iterindex].string_literals, text[iterindex].temp, step)
+        raise ImportError(errMsg)
+
+    assert(isinstance(text,list))
+    text = [line.integral for line in text]
+    text = "\n".join(text)
+    current_file_input = text
+    return text
+
+def stage(filename=None, verbal=False):
     global current_file_input
     with open(filename,"r") as f:
         text = f.readlines()
-        text = [line.strip() for line in text]  # Remove all leading/trailing whitespace from each line
-        text = filter(nonempty,text)            # Only preserve nonempty lines
-        text = filter(filter_comment,text)      # Only preserve non-comments
-        text = [MoguString("script",line) for line in text] # Convert all lines to MoguStrings
-        text = [line.translate("integral") for line in text]# Convert all text to integral representation
-
-        if verbal:
-            for index,line in enumerate(text): print("%d:\t%s"%(index,line))
-
-        text = "\n".join(text)  # Create full text 
+        text = preprocess(text)
         current_file_input = text # Preserver in global namespace for debugging purposes
     return text
 
@@ -41,6 +84,19 @@ def parse_root(filename):
                 "\nCannot parse file: %s\n\nImport halted. No changes made.\n\n" % filename
             )
             raise e
+
+def import_raw(text, verbal=False):
+    SharedData.ActiveFile = "Raw Input"
+    text = preprocess(text,verbal)
+    if verbal:
+        sys.stderr.write("Importing from raw input")
+    try:
+        return RootConsumer.parse(text)
+    except Exception as e:
+        syst.stderr.write(
+                "\nCannot parse text. Import halted. No changes made.\n\n" % filename
+        )
+        raise e
 
 
 def import_file(filename, verbal=False):
