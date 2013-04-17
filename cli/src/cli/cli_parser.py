98147e236ac8fab8738fa26e740d276153bea8f0
Tom Thorogood <tom@tomthorogood.com>
2013-04-17 10:01:08 -0400

    Rewrite final importer piece, including RedisWriter, to work with dynamic databases

--- cli/src/cli/cli_parser.py
+++ cli/src/cli/cli_parser.py
@@ -1,8 +1,15 @@
 from ConfigParser import SafeConfigParser
-from ConfigParser import NoOptionError
 from argparse import ArgumentParser
+import os
 from mogupy_conf import *
 from exceptions import *
+
+CURDIR = os.getcwd()
+
+# TODO
+# This will be replaced upon install within the build
+# directory to instead use the config installation directory
+DB_CONFIG_DIRECTORY = CURDIR
 
 def getCLIArgs():
     global CONFIG_FILE    
@@ -22,43 +29,10 @@ def getCLIArgs():
             type=str, 
             action="store", 
             help="The command you want to run (see readme for available commands")
+    
+    parser.add_argument("--dbconfig", dest="dbconfig",
+            default=os.path.join(CURDIR,"dbconfig.conf")
 
-# Options for configuring your Redis connection
-    parser.add_argument(
-            "--redis-select", 
-            type=int, dest="redis_db", 
-            action="store", 
-            default=config.get("redis_instance","database"), 
-            help="Optional Redis database number (default is read from config file)")
-
-    parser.add_argument(
-            "--redis-port", 
-            type=int, dest="redis_port", 
-            action="store", 
-            default=config.get("redis_instance","port"),
-            help="Optional Redis port (default is read from config file)")
-
-    parser.add_argument(
-            "--redis-host", 
-            type=str, 
-            dest="redis_host", 
-            action="store", 
-            default=config.get("redis_instance","host"), 
-            help="Optional Redis host (default is read from config file")
-
-    parser.add_argument(
-            "--verbal",
-            dest = "v",
-            action = "store_true",
-            default=False,
-            help="Mogu will talk to you more if you turn on this flag.")
-
-    parser.add_argument(
-            "--redis-auth", 
-            type=str, 
-            dest="redis_auth", 
-            action="store", 
-            default=auth,help="Optional Redis auth password (can be set in config file)")
 
 # Boolean options for interacting with the Redis environment
     parser.add_argument("--redis-flush", 
@@ -78,12 +52,5 @@ def getCLIArgs():
             dest="testing", action="store_true", default=False, 
             help="Displays various information depending on the command, making no changes to your database.")
 
-    parser.add_argument("--merge",
-            dest="merge", action="store_true", default=False,
-            help="Will not delete nodes before setting values, which could result in unexpected behaviour, but can make things easier when adding one-offs.")
 
-    parser.add_argument("-o",
-            dest="output", action="store", default=None,
-            help="Will write to this file on export if defined, otherwise will write to stderr")
-
-    return parser.parse_args(), config
+    return parser.parse_args()
---
+++ cli/src/moguio/moguio/ScriptToRedis.py
@@ -0,0 +1,71 @@
+# TODO : Read db info from dbconfig.conf
+
+import PythonObjectConverter
+import SymbolRegistry
+import FileImporter
+import PathImporter
+
+def display_undefined_symbols(registry):
+    output = ""
+    for symbol in registry:
+        if not registry[symbol]:
+            output += "\n"
+            output += str(registry[symbol])
+            output += "\n\tReferenced in: \n")
+            for reference in registry[symbol]:
+                output += "\t\t%s\n" % reference
+    return output
+
+def mogu_import(args):
+    results = []    # Holds the final result of all consumption operations
+    redis_objects = [] #Holds the objects that can be written to Redis
+    write = not args.testing # If testing, don't actually write to Redis
+
+    for path in args.command[1:]
+        if path.endswith(".mogu"): # If the path points to a specific file
+            results.extend(FileImporter.import_file(path.args.v))
+        else:   # The path is a directory
+            results.extend(PathImporter.import_path(path.args.v))
+
+    converter = PythonObjectConverter.PythonObjectConverter()
+    for result in results:
+        redis_objects.extend(converter.convert(result))
+
+    # SANITY CHECKS #
+
+    # First, make sure that all symbols referenced are defined.
+    for registry in [
+            SymbolRegistry.widgetRegistry,
+            SymbolRegistry.templateRegistry,
+            SymbolRegistry.dataRegistry,
+            SymbolRegistry.validatorRegistry,
+            SymbolRegistry.policyRegistry
+            ]:
+        if not registry: # Returns false if a symbol in the registry is not defined
+            sys.stderr.write(display_undefined_symbols(registry))
+            sys.stderr.write("\n== REFUSING TO CONTINUE ==\n")
+            sys.exit()
+
+        if registry.nonreferenced(): # Warns the user if something was defined but never used
+            i = None
+            sys.stderr.write("\n== WARNING: %s contains the following symbols that are defined but never referenced == \n" %
+                    registry.label)
+            for symbol in registry.nonreferenced():
+                sys.stderr.write("\t- %s\n" % symbol)
+            # Give the user a chance to halt the import and fix the problem
+            if not args.yes:
+                i = raw_input("Continue anyway? [y to continue, anything else to cancel]: ")
+            if i != 'y' and not args.yes:
+                sys.stderr.write("Exiting...")
+                sys.exit()
+
+    # Sanity checks complete. Now the process of actually writing
+    # to Redis 
+
+    # TODO Don't forget to make RedisWriter deal with purging/merging
+    if write: 
+        writer = RedisWriter.RedisWriter(args) #TODO RedisWriter should read dbconfig.conf instead
+        if args.v:
+            sys.stderr.write("Writing imported files to database!\n")
+        writer.write(redis_objects)
+
