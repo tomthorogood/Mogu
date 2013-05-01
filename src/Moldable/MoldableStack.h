fc44bd6c8120da4ea9ed8a6ed86a40a939a88447
Tom Thorogood <tom@tomthorogood.com>
2013-05-01 10:16:25 -0400

    MoldableStack upgraded to allow children to be added/removed by external sources.

--- src/Moldable/MoldableStack.h
+++ src/Moldable/MoldableStack.h
@@ -9,13 +9,26 @@
 #define MOLDABLESTACK_H_
 
 #include "MoldableAbstractParent.h"
-#include <Mogu.h>
 #include <Wt/WStackedWidget>
+
+class WFriendlyStack : public Wt::WStackedWidget
+{
+public:
+    inline virtual void addChild(Moldable* child) {
+        Wt::WStackedWidget::addChild(
+                static_cast<Wt::WWebWidget*>(child));
+    }
+
+    inline virtual void removeChild(Moldable* child) {
+        Wt::WStackedWidget::removeChild(
+                static_cast<Wt::WWebWidget*>(child));
+    }
+};
 
 class MoldableStack : public MoldableAbstractParent
 {
 private:
-    Wt::WStackedWidget* __stack;
+    WFriendlyStack* __stack;
     Wt::Signal <> __stack_index_changed;
 
 public:
@@ -24,9 +37,11 @@ public:
     //Stacked containers do not have textual values
     inline virtual std::string moldableValue() { return EMPTY; }
     inline virtual void setMoldableValue(const std::string& str) {}
+
     virtual void appendChild(Moldable* child) {
         __stack->addChild(child);
     }
+
     inline virtual void addWidget(Wt::WWidget* moldable)
     {
         __stack->addWidget(moldable);
@@ -45,7 +60,15 @@ public:
     inline virtual void __init__()
     {
         MoldableAbstractParent::__init__();
-        __stack = new Wt::WStackedWidget();
+        __stack = new WFriendlyStack();
+    }
+
+    inline virtual void addChild(Moldable* child) override {
+        __stack->addChild(child);
+    }
+
+    inline virtual void removeChild(Moldable* child) override {
+        __stack->removeChild(child);
     }
 };
 
