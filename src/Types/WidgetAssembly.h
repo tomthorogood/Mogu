0e7c34dcb99142afe57344219bf0a896e2437aff
Tom Thorogood <tom@tomthorogood.com>
2013-07-25 09:38:46 -0700

    Add support for anonymous_widget storage and assignment operator

--- src/Types/WidgetAssembly.h
+++ src/Types/WidgetAssembly.h
@@ -12,9 +12,15 @@ struct WidgetAssembly
     std::map <int,NodeValue> attrdict;
     std::vector <std::string> children;
     std::vector <std::string> triggers;
+    
+    /* Anonymous children are those created due to 'member' templates that
+     * don't actually live anywhere in the database. These will be assigned
+     * names on the fly at runtime, and registered like any other widget, but
+     * all interactions will be abstracted and handled by their templates.
+     */
+    std::vector <WidgetAssembly> anonymous_children;
     std::string node = "";
     std::string tmpl = "";
-    bool tmpl_stored_events   = false;
     TriggerMap* triggerMap = nullptr;
 
     void setTriggerPrefix(Prefix prefix)
@@ -24,6 +30,18 @@ struct WidgetAssembly
         if (triggerMap != nullptr) delete triggerMap;
         triggerMap = new TriggerMap(triggers.size(), prefix, triggerNode);
     }
+
+    WidgetAssembly& operator=(const WidgetAssembly& other)
+    {
+        attrdict = other.attrdict;
+        children = other.children;
+        triggers = other.triggers;
+        anonymous_children = other.anonymous_children;
+        node = other.node;
+        tmpl = other.tmpl;
+        triggerMap = other.triggerMap;
+    }
+
 };
 
 #endif
