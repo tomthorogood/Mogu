95303a0ef74b6f3927d30fb7907ebc979d6a73ba
Tom Thorogood <tom@tomthorogood.com>
2013-07-26 11:22:22 -0700

    Upgrade from cityhash to openssl SHA 256

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,82 +1,22 @@
 #include <hash.h>
-#include <city.h>
+#include <openssl/sha.h>
 #include <sstream>
 
 namespace Hash {
 
-HashedString::HashedString()
-{
-}
-HashedString::~HashedString()
-{
-}
-
-HashedString64::HashedString64()
-    : HashedString()
-{
-    hashedValue = 0;
-}
-HashedString64::~HashedString64()
-{
-}
-
-inline HashedString64& HashedString64::set(
-    uint64 _value)
-{
-    hashedValue = _value;
-    return *this;
-}
-
-inline std::string HashedString64::as_string()
-{
-    std::stringstream stream;
-    stream << hashedValue;
-    return stream.str();
-}
-
-HashedString128::HashedString128()
-    : HashedString()
-{
-}
-
-HashedString128::~HashedString128()
-{
-}
-;
-
-HashedString128& HashedString128::set( uint128 _value)
-{
-    hashedValue = _value;
-    return *this;
-}
-
-std::string HashedString128::as_string()
-{
-    std::stringstream stream;
-    stream << hashedValue.first << hashedValue.second;
-    return stream.str();
-}
-
-HashedString* hashedString( const std::string& str)
-{
-    size_t len = (size_t) str.length();
-    if (len > MAX_64_HASH_LENGTH) {
-        HashedString128* hash = new HashedString128();
-        hash->set(CityHash128(str.c_str(), len));
-        return hash;
-    }
-
-    HashedString64* hash = new HashedString64();
-    hash->set(CityHash64(str.c_str(), len));
-    return hash;
-}
-
 std::string toHash( const std::string& str)
 {
-    HashedString* hash = hashedString(str);
-    std::string ret = hash->as_string();
-    delete hash;
-    return ret;
+std::string toHash(std::string str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash,&sha256);
+    std::stringstream buf;
+    for (int i =0; i < SHA256_DIGEST_LENGTH; ++i)
+        buf << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    return buf.str();
 }
 
 }    //namespace hash
--- src/hash.h
+++ src/hash.h
@@ -13,66 +13,6 @@
 
 namespace Hash {
 
-/*!\brief Recommended by the City Hash developers. */
-#define MAX_64_HASH_LENGTH 2000
-
-/*!\brief A base polymorphic class to obtain hashed values from the City Hash library. */
-class HashedString
-{
-public:
-    HashedString();
-    virtual ~HashedString();
-
-    /*!\brief Returns the string representation of the hashed value. */
-    virtual std::string as_string() =0;
-};
-
-/*!\brief The 64-bit implementation of the HashedString base class,
- * deals with uint64 values.
- */
-class HashedString64: public HashedString
-{
-private:
-    uint64 hashedValue;
-public:
-    HashedString64();
-    virtual ~HashedString64();
-    HashedString64& set(
-        uint64 _value);
-    std::string as_string();
-};
-
-/*!\brief The 128-bit implementation of the HashedString base class,
- * which deals with uint128 values.
- */
-class HashedString128: public HashedString
-{
-private:
-    uint128 hashedValue;
-public:
-    HashedString128();
-    virtual ~HashedString128();
-    HashedString128& set(
-        uint128 _value);
-    std::string as_string();
-};
-
-/*!\brief Returns a pointer to a HashedString object. Note that this
- * pointer MUST be deleted in order to avoid memory leakage!
- * @param str The string to be hashed.
- * @return A pointer to a HashedString object.
- */
-HashedString* hashedString(
-    const std::string& str);
-
-/*!\brief hashes a string of any length! Utilizes the City Hash lib due to its
- * high performance rating and the fact that cryptography is not an issue for
- * these purposes. If the string passed in is greater than 2000 characters, the
- * CityHash64 function will be used, per the recommendation of the City Hash
- * authors.
- * @param str The string to be hashed.
- * @return The hashed value.
- */
 std::string toHash(const std::string& str);
 
 }    //<-- namespace hash
